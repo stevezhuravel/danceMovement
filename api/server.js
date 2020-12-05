9d6b0285f87dceb197257cbeb74beb65356eec59
stevezhuravel <stevezhuravel@gmail.com>
2020-12-05 00:17:06 -0500

    refactoring and database

--- api/server.js
+++ api/server.js
@@ -4,12 +4,12 @@ const app = express();
 const mysql = require('mysql');
 
 // Database configurations
-const pool  = mysql.createPool({
-		connectionLimit : 100, //arbitrary value
-		host     : 'localhost',
-		user     : 'dance',
-		password : '1111',
-		database : 'dancemovement'
+const pool = mysql.createPool({
+    connectionLimit: 100, //arbitrary value
+    host: 'localhost',
+    user: 'dance',
+    password: '1111',
+    database: 'dancemovement'
 });
 
 var path = require('path');
@@ -25,65 +25,65 @@ app.use(cors());
 	 ----
 */
 
+app.use('../client/static', express.static('static'))
+    // GET all the users
+app.get("/api/v1/users", (req, res) => {
+    try {
+        let query = mysql.format('SELECT * FROM users');
+        pool.query(query, (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
 
-// GET all the users
-app.get("/api/v1/users",(req,res) => {
-	try {
-		let query = mysql.format('SELECT * FROM users');
-		pool.query(query,(err, data) => {
-			if(err) {
-				console.error(err);
-				return;
-			}
+            // rows fetch
+            console.log(data);
+            res.json(data);
+        });
 
-			// rows fetch
-			console.log(data);
-			res.json(data);
-		});
-
-	} catch (error) {
-		console.log(error.message);
-	}
-				});
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 
 
 // GET a single user that matches our id
-app.get("/api/v1/users/:id",(req,res) => {
-	try {
-		const { id } = req.params;
-		let query = mysql.format('SELECT * FROM users WHERE usrname=$1',[id]);
-		pool.query(query,(err, data) => {
-			if(err) {
-				console.error(err);
-				return;
-			}
+app.get("/api/v1/users/:id", (req, res) => {
+    try {
+        const { id } = req.params;
+        let query = mysql.format('SELECT * FROM users WHERE usrname=$1', [id]);
+        pool.query(query, (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
 
-			// rows fetch
-			console.log(data);
-			res.json(data);
-		});
+            // rows fetch
+            console.log(data);
+            res.json(data);
+        });
 
-	} catch (error) {
-		console.log(error.message);
-	}
+    } catch (error) {
+        console.log(error.message);
+    }
 });
 
-app.get("/api/v1/media",(req,res) => {
-	try {
-		const { id } = req.params;
-		let query = mysql.format('SELECT * FROM media');
-		pool.query(query,(err, data) => {
-		if(err) {
-			console.error(err);
-			return;
-		}
-		// rows fetch
-		console.log(data);
-		res.json(data);
-		});
-	} catch (error) {
-		console.log(error.message);
-	}
+app.get("/api/v1/media", (req, res) => {
+    try {
+        const { id } = req.params;
+        let query = mysql.format('SELECT * FROM media');
+        pool.query(query, (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            // rows fetch
+            console.log(data);
+            res.json(data);
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
 });
 
 // ----
@@ -97,92 +97,89 @@ app.get("/api/v1/media",(req,res) => {
 		 }
 }
 */
-app.put('/api/v1/user/new', async (req, res) => {
-	try {
-		// const { username, password } = req.params;
-		const { username, password } = req.body.data;
-		let query = mysql.format('INSERT INTO ?? (??,??) VALUES (?,?)',
-								 ["users", "usrname", "password", username, password]);
-		pool.query(query,(err, response) => {
-			if(err) {
-				console.error(err);
-				return;
-			}
-			// rows fetch
-			console.log(response);
-			res.send({ status: 'success', message: 'user added' });
-		});
+app.put('/api/v1/user/new', async(req, res) => {
+    try {
+        // const { username, password } = req.params;
+        const { username, password } = req.body.data;
+        let query = mysql.format('INSERT INTO ?? (??,??) VALUES (?,?)', ["users", "usrname", "password", username, password]);
+        pool.query(query, (err, response) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            // rows fetch
+            console.log(response);
+            res.send({ status: 'success', message: 'user added' });
+        });
 
-	} catch (error) {
-		console.log(error.message);
-	}
+    } catch (error) {
+        console.log(error.message);
+    }
 
 })
 
 // Insert new media to database
-app.put('/api/v1/media/new', async (req, res) => {
-		try {
-				const { name } = req.params;
-				let query = mysql.format('INSERT INTO ?? (??) VALUES (?)',
-																	["media", "name", name]);
-				pool.query(query,(err, response) => {
-						if(err) {
-								console.error(err);
-								return;
-						}
-						// rows fetch
-						console.log(response);
-						res.send({ status: 'success', message: 'media added' });
-				});
+app.put('/api/v1/media/new', async(req, res) => {
+    try {
+        const { name } = req.params;
+        let query = mysql.format('INSERT INTO ?? (??) VALUES (?)', ["media", "name", name]);
+        pool.query(query, (err, response) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            // rows fetch
+            console.log(response);
+            res.send({ status: 'success', message: 'media added' });
+        });
 
-		} catch (error) {
-				console.log(error.message);
-		}
+    } catch (error) {
+        console.log(error.message);
+    }
 
 })
 
 
 // Insert new playlist to database
-app.put('/api/v1/playlist/new', async (req, res) => {
-		try {
-				const { user, media_list } = req.params;
-				let query = mysql.format('INSERT INTO ?? (??) VALUES (?)',
-																	["playlist", "usr_id", "media_list", user, media_list]);
-				pool.query(query,(err, response) => {
-						if(err) {
-							console.error(err);
-							return;
-						}
+app.put('/api/v1/playlist/new', async(req, res) => {
+    try {
+        const { user, media_list } = req.params;
+        let query = mysql.format('INSERT INTO ?? (??) VALUES (?)', ["playlist", "usr_id", "media_list", user, media_list]);
+        pool.query(query, (err, response) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
 
-					// rows fetch
-						console.log(response);
-						res.send({ status: 'success', message: 'playlist added' });
-				});
+            // rows fetch
+            console.log(response);
+            res.send({ status: 'success', message: 'playlist added' });
+        });
 
-		} catch (error) {
-				console.log(error.message);
-		}
+    } catch (error) {
+        console.log(error.message);
+    }
 });
 
 app.get('/', function(req, res) {
-		res.sendFile(path.join(__dirname + '/views/index.html'));
+    res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 app.get('/signup', function(req, res) {
-		res.sendFile(path.join(__dirname + '/views/signup.html'));
+    res.sendFile(path.join(__dirname, '../client/signup.html'));
 });
 
 app.get('/login', function(req, res) {
-		res.sendFile(path.join(__dirname + '/views/sign-in.html'));
+    res.sendFile(path.join(__dirname, '../client/sign-in.html'));
 });
 
-app.get('/password-reset', function(req, res){
-		res.sendFile(path.join(__dirname + '/views/passwordreset.html'));
+app.get('/password-reset', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/passwordreset.html'));
 });
 
-app.use('/static', express.static('static'))
+
 
 // launches the server
 app.listen(5000, () => {
-		console.log('Server is running at port 5000');
+    console.log('Server is running at port 5000');
 });
